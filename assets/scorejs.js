53be12dd299c8d04313b363abcd922ba48bbb8c8
miraj <miraj00@gmail.com>
2021-07-12 01:28:26 -0400

    score page and final page issue fixed

--- assets/scorejs.js
+++ assets/scorejs.js
@@ -1,11 +1,14 @@
 
-//-------Page 1 style set -------------------------------------------
+var quizCompleteEl = document.querySelector("#quiz-complete");
+quizCompleteEl.setAttribute("style", "text-align:center; margin: 100px auto 0 auto; padding-bottom: 20px;");
 
-var mainEl = document.querySelector("#quiz-complete");
-mainEl.setAttribute("style", "text-align:center; margin: 100px auto 0 auto; padding-bottom: 20px;");
+var currentScoreEl = document.querySelector("#current-score");
+currentScoreEl.textContent = 'Your Score is :' ;
+currentScoreEl.setAttribute("style", "margin: auto; text-align:center; padding-bottom: 40px;");
 
-var instructionEl = document.querySelector("#current-score");
-instructionEl.setAttribute("style", "margin: auto; text-align:center; padding-bottom: 40px;");
+var btnSubmit = document.querySelector("#btn-submit");
+btnSubmit.setAttribute("style", "margin: 50px auto 0 900px; background-color: blue; color: white; font-size: 40px;");
 
-var btnStart = document.querySelector("#btn-submit");
-btnStart.setAttribute("style", "margin: 50px auto 0 900px; background-color: blue; color: white; font-size: 40px;");
+// var x = localStorage.getItem(Score)
+// console.log(x); 
+// document.getElementById("demo").innerHTML = x;
